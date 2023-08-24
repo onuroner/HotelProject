c4415b674d1673175bc7139677fb8d05236b1102
Onur Öner <onuroner4054@gmail.com>
2023-08-24 17:22:30 +0300

    13. Defining managers

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/SubscriptionManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class SubscriptionManager : ISubscribeService
+    {
+        private readonly ISubscribeDal _subscribeDal;
+
+        public SubscriptionManager(ISubscribeDal subscribeDal)
+        {
+            _subscribeDal = subscribeDal;
+        }
+
+        public void TDelete(Subscribe entity)
+        {
+            _subscribeDal.Delete(entity);
+        }
+
+        public List<Subscribe> TGetAll()
+        {
+            return _subscribeDal.GetAll();
+        }
+
+        public Subscribe TGetByID(int id)
+        {
+            return _subscribeDal.GetByID(id);
+        }
+
+        public void TInsert(Subscribe entity)
+        {
+            _subscribeDal.Insert(entity);
+        }
+
+        public void TUpdate(Subscribe entity)
+        {
+            _subscribeDal.Update(entity);
+        }
+    }
+}
