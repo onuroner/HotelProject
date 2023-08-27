22d31f4a7301b8ada9d6487e05aedc07b6777a8a
Onur Öner <onuroner4054@gmail.com>
2023-08-27 19:44:30 +0300

    23. Listing staf on ui

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -35,6 +35,14 @@ namespace HotelProject.WebApi
             builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
             builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy("OtelApiCors", opts =>
+                {
+                    opts.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -45,7 +53,7 @@ namespace HotelProject.WebApi
             }
 
             app.UseAuthorization();
-
+            app.UseCors("OtelApiCors");
 
             app.MapControllers();
 
--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -8,6 +8,7 @@ namespace HotelProject.WebUI
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpClient();
 
             var app = builder.Build();
 
